ef9d07d79ec656cb8e822caec269385c49b111bf
mike-j-king <mike@mjkdigital.com.au>
2019-03-12 19:25:30 +1100

    modifying mysql connection to use pool connection

--- app/model/db.js
+++ app/model/db.js
@@ -3,15 +3,12 @@
 var mysql = require('mysql');
 
 // connection to heroku mysql db
-var connection = mysql.createConnection({
+var pool = mysql.createPool({
+    connectionLimit: 10,
     host: 'us-cdbr-iron-east-03.cleardb.net',
     user: 'b756c5a53c8888',
     password: 'e12cc1cb',
     database: 'heroku_75d49d35a82afaa'
 });
 
-connection.connect(function(err) {
-    if (err) throw err;
-});
-
-module.exports = connection;
+module.exports = pool;
