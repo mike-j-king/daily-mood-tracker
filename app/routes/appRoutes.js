c6a186dfdd36cc35db48de81a827b0a3f6c513b5
mike-j-king <mike@mjkdigital.com.au>
2019-03-12 15:13:15 +1100

    setting up server and api endpoints

---
+++ app/routes/appRoutes.js
@@ -0,0 +1,11 @@
+'use strict';
+module.exports = function(app) {
+  var dailyMoodTracker = require('../controller/appController');
+
+  app.route('/moods')
+    .get(dailyMoodTracker.list_all_moods)
+    .post(dailyMoodTracker.create_mood);
+   
+  app.route('/moods/:moodId')
+    .put(dailyMoodTracker.update_mood)
+};
